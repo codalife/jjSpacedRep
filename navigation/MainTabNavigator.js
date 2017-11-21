b704b1b9077f630fc2b82be29933848b5a61f47a
codalife <anuar.zhukenov@gmail.com>
2017-11-21 01:05:41 -0800

    feat video recorder

---
+++ navigation/MainTabNavigator.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Platform } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { TabNavigator, TabBarBottom } from 'react-navigation';
+
+import Colors from '../constants/Colors';
+
+import HomeScreen from '../screens/HomeScreen';
+import Recorder from '../screens/Recorder';
+import EnterData from '../screens/EnterData';
+
+export default TabNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+    },
+    Recorder: {
+      screen: Recorder,
+    },
+    EnterData: {
+      screen: EnterData,
+    },
+  },
+  {
+    navigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused }) => {
+        const { routeName } = navigation.state;
+        let iconName;
+        switch (routeName) {
+          case 'Home':
+            iconName =
+              Platform.OS === 'ios'
+                ? `ios-information-circle${focused ? '' : '-outline'}`
+                : 'md-information-circle';
+            break;
+          case 'Recorder':
+            iconName =
+              Platform.OS === 'ios' ? `ios-options${focused ? '' : '-outline'}` : 'md-options';
+          case 'EnterData':
+            iconName =
+              Platform.OS === 'ios' ? `ios-options${focused ? '' : '-outline'}` : 'md-options';
+        }
+        return (
+          <Ionicons
+            name={iconName}
+            size={28}
+            style={{ marginBottom: -3 }}
+            color={focused ? Colors.tabIconSelected : Colors.tabIconDefault}
+          />
+        );
+      },
+    }),
+    tabBarComponent: TabBarBottom,
+    tabBarPosition: 'bottom',
+    animationEnabled: false,
+    swipeEnabled: false,
+  }
+);
---
+++ screens/Recorder.js
@@ -0,0 +1,114 @@
+import React from 'react';
+import { Animated, Text, View, TouchableOpacity, Image, CameraRoll } from 'react-native';
+import { Camera, Permissions, Video } from 'expo';
+import { Ionicons } from '@expo/vector-icons';
+
+export default class Recorder extends React.Component {
+  state = {
+    hasCameraPermission: null,
+    type: Camera.Constants.Type.back,
+    recording: false,
+    animatedStartValue: new Animated.Value(1),
+    color: 'green',
+  };
+
+  async componentWillMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({ hasCameraPermission: status === 'granted' });
+  }
+
+  render() {
+    const { hasCameraPermission } = this.state;
+    if (hasCameraPermission === null) {
+      return <View />;
+    } else if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>;
+    } else {
+      let video;
+      if (this.state.videoUri) {
+        video = <Video source={{ uri: this.state.videoUri }} style={{ width: 250, height: 250 }} />
+      } else {
+        video = <Text>Nothing yet</Text>;
+      }
+
+      return (
+        <View style={{ flex: 1 }}>
+          <Camera 
+            style={{ flex: 1 }} 
+            type={this.state.type}
+            ref={ref => { this.camera = ref; }}>
+            <View
+              style={{
+                flex: 1,
+                backgroundColor: 'transparent',
+                alignContent: 'center',
+                flexDirection: 'row',
+              }}>
+              <TouchableOpacity
+                style={{
+                  flex: 0.1,
+                  alignSelf: 'flex-end',
+                  alignItems: 'center',
+                }}
+                onPress={() => {
+                  this.record();
+                }}>
+                <Animated.View style={{
+                    opacity: this.state.animatedStartValue
+                  }} >
+                  <Ionicons name="ios-videocam" size={32} color={this.state.color} />
+                </Animated.View>
+              </TouchableOpacity>
+            </View>
+          </Camera>
+        </View>
+      );
+    }
+  }
+
+  record = async () => {
+    this.setState({
+      recording: !this.state.recording
+    })
+    if (this.camera && !this.state.recording) {
+
+      this.setState({
+        recording: true,
+        color: 'blue'
+      });
+
+      const blink = Animated.loop(
+        Animated.sequence([
+          Animated.timing(
+            this.state.animatedStartValue,
+            {
+              toValue: 0,
+              duration: 1000,
+            }
+          ),
+          Animated.timing(
+            this.state.animatedStartValue,
+            {
+              toValue: 1,
+              duration: 500,
+            }
+          )
+        ]) 
+      )
+
+      blink.start();
+
+      let video = await this.camera.recordAsync();
+
+      this.setState({
+        color: 'green'
+      });
+
+      blink.stop();
+
+      this.props.navigation.navigate('EnterData', { uri: video.uri });
+    } else {
+      this.camera.stopRecording();
+    }
+  }
+}
